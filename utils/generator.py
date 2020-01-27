76e7fa12e50b9027006db2f61a774d9a1ae6bd8a
teamclouday <teamclouday@gmail.com>
2020-01-27 10:50:55 -0500

    update

--- utils/generator.py
+++ utils/generator.py
@@ -1,1 +1,11 @@
 # this file contains codes for generating missing values in a dataset
+
+import random
+import pandas as pd
+from utils.data import *
+
+# generate by complete random
+def gen_complete_random(data, random_ratio=0.3):
+    if random_ratio > 0.5:
+        print("Warning: gen_complete_random, random missing ratio > 0.5")
+    
