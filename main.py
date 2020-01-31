4e6ed19ea63afe1d54ad30f47b55deb73bb5b6db
teamclouday <teamclouday@gmail.com>
2020-01-30 19:22:57 -0500

    add function

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 # this file contains codes that test various research targets
 
+import shutil
 from utils.data import *
 from utils.generator import *
+from utils.completer import *
+
+def printBar():
+    w, _ = shutil.get_terminal_size()
+    w -= 1
+    print("-"*w)
 
 def test():
     data = create_test_dataset()
-    print("Original Dataset:")
     print(data.X.to_string())
+    printBar()
     data_c_r = gen_complete_random(data)
-    print("Complete Random:")
     print(data_c_r.X.to_string())
-    data_r = gen_random(data)
-    print("Random:")
-    print(data_r.X.to_string())
+    printBar()
+    data_complete = complete_by_nearby_row(data_c_r)
+    print(data_complete.X.to_string())
 
 def test_iris():
     data = create_iris_dataset()
@@ -22,4 +28,4 @@ def test_iris():
 
 if __name__ == "__main__":
     dataset_prepare()
-    test_iris()
+    test()
--- utils/completer.py
+++ utils/completer.py
@@ -9,7 +9,7 @@ from utils.data import *
 # fill all missing entries with one value
 def complete_by_value(data, value=0):
     data = data.copy()
-    data.X.fillna(value)
+    data.X.fillna(value, inplace=True)
     return data
 
 # Method 2
@@ -20,14 +20,15 @@ def complete_by_mean_col(data):
     values = {}
     for name in names:
         values[name] = data.X[name].mean()
-    data.X.fillna(values)
+    data.X.fillna(values, inplace=True)
     return data
 
 # Method 3
 # complete missing entries using value from previous row
-def compete_by_nearby_row(data):
+def complete_by_nearby_row(data):
     data = data.copy()
-
+    data.X.fillna(method="ffill", inplace=True)
+    return data
 
 # Method 4
 # train a regression model and predict the target missing value
