4ab12d2da1cf5dd677255d9e4eb081434784f217
teamclouday <teamclouday@gmail.com>
2020-02-17 22:29:02 -0500

    update

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ from utils.test import *
 
 if __name__ == "__main__":
     dataset_prepare()
-    machine = TestMachine(create_iris_dataset, gen_complete_random, random_gen=10, predictor_cv=10, record_time=True)
-    machine.run()
-    machine.plot_compare_models(save_file_name="iris1.png")
-    machine.plot_compare_completers(save_file_name="iris2.png")
-    # machine = TestMachine(create_bank_dataset, gen_complete_random, predictor_cv=10, record_time=True)
+    # machine = TestMachine(create_iris_dataset, gen_complete_random, random_gen=10, predictor_cv=10, record_time=True)
     # machine.run()
-    # machine.plot_compare_models(save_file_name="bank1.png")
-    # machine.plot_compare_completers(save_file_name="bank2.png")
+    # machine.plot_compare_models(save_file_name="iris1.png")
+    # machine.plot_compare_completers(save_file_name="iris2.png")
+    machine = TestMachine(create_bank_dataset, gen_complete_random, random_gen=5, predictor_cv=10, record_time=True)
+    machine.run()
+    machine.plot_compare_models(save_file_name="bank1.png")
+    machine.plot_compare_completers(save_file_name="bank2.png")
