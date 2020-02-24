967b0d4948e510d64dd6f3c269b5096c021a0d35
teamclouday <teamclouday@gmail.com>
2020-02-23 22:51:57 -0500

    add 3 more dataset

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ In the process of machine learning, input data always plays an essential role in
 However, real world data is usually not perfect. Very often part of the dataset is missing.  
 This repo is created to research on different methods of data replacing and guessing, and their according effect to several well-known machine learning algorithms.  
 
+Specifically, this research is trying to find out how the missing data would make the output of machine learning algorithms "biased".  
+
 ------
 
 ### Main Variables  
@@ -17,9 +19,13 @@ This repo is created to research on different methods of data replacing and gues
 1. [Iris Dataset](https://archive.ics.uci.edu/ml/datasets/Iris) (UCI)  
 2. [Bank Dataset](https://archive.ics.uci.edu/ml/datasets/Bank+Marketing) (UCI)  
 3. [Adult Dataset](https://archive.ics.uci.edu/ml/datasets/Adult) (UCI)  
+4. [Compas Dataset](https://github.com/propublica/compas-analysis/)  
+5. [Heart Disease Dataset](https://archive.ics.uci.edu/ml/datasets/Heart+Disease) (UCI)  
+6. [Drug Consumption Dataset](https://archive.ics.uci.edu/ml/datasets/Drug+consumption+%28quantified%29) (UCI)  
 
 ------
 
 ### References  
 1. [Missing-data imputation](http://www.stat.columbia.edu/~gelman/arm/missing.pdf)  
-2. [Multiple Imputation in Stata](https://stats.idre.ucla.edu/stata/seminars/mi_in_stata_pt1_new/)
+2. [Multiple Imputation in Stata](https://stats.idre.ucla.edu/stata/seminars/mi_in_stata_pt1_new/)  
+3. [COMPAS Recidivism Risk Score Data and Analysis](https://www.propublica.org/datastore/dataset/compas-recidivism-risk-score-data-and-analysis)  
--- utils/data.py
+++ utils/data.py
@@ -43,17 +43,69 @@ def _dataset_download_adult(folder):
         urllib.request.urlretrieve(URL_PATH + name, os.path.join(folder, name))
     print("Adult dataset is downloaded")
 
+# download compas dataset
+def _dataset_download_compas(folder):
+    URL_PATH = "https://raw.githubusercontent.com/propublica/compas-analysis/master/"
+    FILE_NAMES = ["compas-scores-two-years.csv", "compas-scores-two-years-violent.csv", "compas-scores.csv", "cox-parsed.csv", "cox-violent-parsed.csv"]
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    for name in FILE_NAMES:
+        urllib.request.urlretrieve(URL_PATH + name, os.path.join(folder, name))
+    print("Compas dataset is downloaded")
+
+# download heart dataset
+def _dataset_download_heart(folder):
+    URL_PATH = "https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/"
+    FILE_NAMES = ["heart-disease.names", "new.data"]
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    for name in FILE_NAMES:
+        urllib.request.urlretrieve(URL_PATH + name, os.path.join(folder, name))
+    # convert new.data
+    outFile = open(os.path.join(folder, "converted.data"), "w")
+    with open(os.path.join(folder, FILE_NAMES[1]), "r") as inFile:
+        new_line = []
+        for line in inFile.readlines():
+            line = line.rstrip().split()
+            if len(line) == 3:
+                line = ",".join(line)
+                new_line.append(line)
+                new_line = ",".join(new_line)
+                print(new_line, file=outFile)
+                new_line = []
+            else:
+                line = ",".join(line)
+                new_line.append(line)
+    outFile.close()
+    print("Heart Disease dataset is downloaded")
+
+# download drug dataset
+def _dataset_download_drug(folder):
+    URL_PATH = "https://archive.ics.uci.edu/ml/machine-learning-databases/00373/"
+    FILE_NAMES = ["drug_consumption.data"]
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    for name in FILE_NAMES:
+        urllib.request.urlretrieve(URL_PATH + name, os.path.join(folder, name))
+    print("Drug Consumption dataset is downloaded")
+
 # function that checks for existence of datasets
 def dataset_prepare():
     dataset_folders = [
         os.path.join("dataset", "iris"),
         os.path.join("dataset", "bank"),
-        os.path.join("dataset", "adult")
+        os.path.join("dataset", "adult"),
+        os.path.join("dataset", "compas"),
+        os.path.join("dataset", "heart"),
+        os.path.join("dataset", "drug")
     ]
     load_functions = [
         _dataset_download_iris,
         _dataset_download_bank,
-        _dataset_download_adult
+        _dataset_download_adult,
+        _dataset_download_compas,
+        _dataset_download_heart,
+        _dataset_download_drug
     ]
     for folder, func in zip(dataset_folders, load_functions):
         if not os.path.exists(folder):
@@ -129,11 +181,62 @@ def create_iris_dataset(print_time=False):
         print("Performance Monitor: ({:.4f}s) ".format(time.process_time() - tt) + inspect.stack()[0][3])
     return Dataset("iris", X, y)
 
+def create_heart_dataset(print_time=False):
+    if print_time:
+        tt = time.process_time()
+    names = {
+        3:"age",
+        4:"sex",
+        9:"cp",
+        10:"trestbps",
+        12:"chol",
+        16:"fbs",
+        19:"restecg",
+        32:"thalach",
+        38:"exang",
+        40:"oldpeak",
+        41:"slope",
+        44:"ca",
+        51:"thal",
+        58:"num"
+    }
+    data = pd.read_csv(os.path.join("dataset", "heart", "converted.data"), names=list(names.values()), usecols=[(x-1) for x in list(names.keys())])
+    X = data.iloc[:, :-1].copy()
+    y = data.iloc[:, -1].copy()
+    if print_time:
+        print("Performance Monitor: ({:.4f}s) ".format(time.process_time() - tt) + inspect.stack()[0][3])
+    return Dataset("heart", X, y)
+
+def create_drug_dataset(print_time=False, target_drug="Heroin"):
+    if print_time:
+        tt = time.process_time()
+    names = [
+        "ID", "Age Range", "Gender", "Education", "Country", "Ethnicity",
+        "Nscore", "Escore", "Oscore", "Ascore", "Cscore", "Impulsive", "SS",
+        "Alcohol", "Amphet", "Amyl", "Benzos", "Caff", "Cannabis", "Choc", "Coke",
+        "Crack", "Ecstasy", "Heroin", "Ketamine", "Legalh", "LSD", "Meth",
+        "Mushrooms", "Nicotine", "Semer", "VSA"
+    ]
+    labels = [
+        "Alcohol", "Amphet", "Amyl", "Benzos", "Caff", "Cannabis", "Choc", "Coke",
+        "Crack", "Ecstasy", "Heroin", "Ketamine", "Legalh", "LSD", "Meth",
+        "Mushrooms", "Nicotine", "Semer", "VSA"
+    ]
+    assert target_drug in labels
+    labels = {x:y for (y,x) in enumerate(labels)}
+    data = pd.read_csv(os.path.join("dataset", "drug", "drug_consumption.data"), names=names)
+    X = data.iloc[:, :-len(labels)].copy()
+    y = data.iloc[:, -(labels[target_drug])].copy()
+    if print_time:
+        print("Performance Monitor: ({:.4f}s) ".format(time.process_time() - tt) + inspect.stack()[0][3])
+    return Dataset("drug_"+target_drug, X, y, convert_all=True)
+
 class Dataset:
-    def __init__(self, name, X, y, auto_convert=True, types=None):
+    def __init__(self, name, X, y, auto_convert=True, types=None, convert_all=False):
         self.name = name
         self.X = X
         self.y = y
+        self.convert_all = convert_all
         if auto_convert:
             self._convert_categories()
         self.types = X.dtypes
@@ -143,9 +246,12 @@ class Dataset:
     def _convert_categories(self):
         columns = self.X.columns
         columns_for_convert = []
-        for col in columns:
-            if not is_numeric_dtype(self.X[col]):
-                columns_for_convert.append(col)
+        if not self.convert_all:
+            for col in columns:
+                if not is_numeric_dtype(self.X[col]):
+                    columns_for_convert.append(col)
+        else:
+            columns_for_convert = columns
         self.encoders = {}
         for col in columns_for_convert:
             encoder = LabelEncoder()
