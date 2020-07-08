8acd46d079968e6321d0a0de1c3be5e60402d443
teamclouday <teamclouday@gmail.com>
2020-07-08 18:37:36 -0400

    update plot scrip

--- script_plot.py
+++ script_plot.py
@@ -21,8 +21,8 @@ PLOT_ADULT_ACC          = False
 PLOT_ADULT_F1           = False
 PLOT_COMPAS_ACC         = False
 PLOT_COMPAS_F1          = False
-PLOT_TITANIC_ACC        = True
-PLOT_TITANIC_F1         = False
+PLOT_TITANIC_ACC        = False
+PLOT_TITANIC_F1         = True
 
 TRANSFORM_OUTPUTS       = True
 
