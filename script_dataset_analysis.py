e8dff9b61fe79d674bf5ba6bea537eefe5bd1f5c
teamclouday <teamclouday@gmail.com>
2020-11-09 22:46:54 -0500

    update script

--- script_dataset_analysis.py
+++ script_dataset_analysis.py
@@ -12,13 +12,16 @@ from utils.data import create_adult_dataset, create_bank_dataset
 from utils.data import create_communities_dataset, create_compas_dataset
 from utils.data import create_german_dataset, create_titanic_dataset
 
-from utils.generator import gen_complete_random
+from utils.generator import gen_complete_random_ext
 from utils.completer import complete_by_mean_col
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.metrics import confusion_matrix, accuracy_score
 from imblearn.over_sampling import SMOTE
+
+PLOT_DROP           = True
+PLOT_IMPUTE         = True
 
 PLOT_ADULT          = True
 PLOT_COMPAS         = True
@@ -59,7 +62,14 @@ def cross_val(data: Dataset, data_config, clf_config, complete_function=None):
                 data_incomplete = Dataset("tmp", X_train, Y_train, types=data.types, 
                     protected_features=data.protected_features, categorical_features=data.categorical_features,
                     encoders=[data.X_encoders, data.y_encoder])
-                data_complete = complete_function(data_incomplete)
+                try:
+                    data_complete = complete_function(data_incomplete)
+                except Exception as e:
+                    print("Error: {}. Skipped".format(e))
+                    continue
+                if data_complete.X.isnull().sum().sum() > 0:
+                    print("Complete function error, skipped")
+                    continue
                 X_train = data_complete.X.copy()
                 Y_train = data_complete.y.copy()
             X_train.drop(columns=data.protected_features, inplace=True)
@@ -68,7 +78,14 @@ def cross_val(data: Dataset, data_config, clf_config, complete_function=None):
                 data_incomplete = Dataset("tmp", X_test, Y_test, types=data.types, 
                     protected_features=data.protected_features, categorical_features=data.categorical_features,
                     encoders=[data.X_encoders, data.y_encoder])
-                data_complete = complete_function(data_incomplete)
+                try:
+                    data_complete = complete_function(data_incomplete)
+                except Exception as e:
+                    print("Error: {}. Skipped".format(e))
+                    continue
+                if data_complete.X.isnull().sum().sum() > 0:
+                    print("Complete function error, skipped")
+                    continue
                 X_test = data_complete.X.copy()
                 Y_test = data_complete.y.copy()
             
@@ -173,19 +190,90 @@ def analysis_drop_correlated_features(data_fn, folder, filename):
     plt.close()
     print("Done")
 
-def analysis_impute_correlated_features():
-    pass
+def analysis_impute_correlated_features(data_fn, folder, filename):
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    with open("params_datasets.json", "r") as inFile:
+        dataConfig = json.load(inFile)
+    with open("params_acc.json", "r") as inFile:
+        clfConfig = json.load(inFile)
+    data: Dataset = drop_na(data_fn())
+    cdata, encoder = convert_protected(data)
+    ccdata = concat(cdata)
+    print("Dataset: {}".format(data.name))
+    dataConfig = dataConfig[data.name]
+    clfConfig = clfConfig[data.name]["LogReg"]
+    correlation = ccdata.corr()[data.protected_features[0]]
+    correlation = correlation[correlation.abs().sort_values(ascending=False).head(15).index]
+    if "_TARGET_" in correlation.keys():
+        del correlation["_TARGET_"]
+    del correlation[data.protected_features[0]]
+    print("Correlation with protected attribute:")
+    for key, val in correlation.items():
+        print("\tFeature: {:<30} Corr: {:.4f}".format(key, val))
+    print("Computing accuracy and bias by MCAR and Mean Imputation")
+    correlated_features = np.array(correlation.keys())
+    plot_bias = []
+    plot_acc = []
+    for i in range(len(correlated_features)):
+        print("Impute on {:<2} most correlated features".format(i))
+        data_tmp = data.copy()
+        # data_tmp.X.drop(columns=correlated_features[range(i)], inplace=True)
+        data_tmp = gen_complete_random_ext(data_tmp, random_ratio=0.2, selected_columns=correlated_features[range(i)])
+        bias, acc = cross_val(data_tmp, dataConfig, clfConfig, complete_by_mean_col)
+        plot_bias.append(bias)
+        plot_acc.append(acc)
+    print("Generating plots")
+    fig, axes = plt.subplots(3, 1, figsize=(5, 16))
+    plot_pos = np.arange(len(correlated_features))
+    axes[0].barh(plot_pos, correlation.abs(), align='center', height=0.5, tick_label=correlated_features)
+    axes[0].set_xlabel("Absolute Correlation with Protected Attribute")
+
+    axes[1].bar(plot_pos, plot_bias, tick_label=[str(i) for i in range(len(correlated_features))], width=0.8, align="center")
+    axes[1].set_xlabel("Features Imputed")
+    axes[1].set_ylabel("Bias")
+    axes[1].set_ylim([0.0, 1.0])
+    for i, val in enumerate(plot_bias):
+        axes[1].text(i, val+0.05, "{:.2f}".format(val), horizontalalignment='center')
+
+    axes[2].bar(plot_pos, plot_acc, tick_label=[str(i) for i in range(len(correlated_features))], width=0.8, align="center")
+    axes[2].set_xlabel("Features Imputed")
+    axes[2].set_ylabel("Accuracy")
+    axes[2].set_ylim([0.0, 1.2])
+    for i, val in enumerate(plot_acc):
+        axes[2].text(i, val+0.05, "{:.2f}".format(val), horizontalalignment='center')
+    fig.suptitle("Most Correlated Feature Impute Experiment ({})".format(data.name))
+    plt.subplots_adjust(top=0.94)
+    fig.savefig(os.path.join(folder, filename), transparent=False, bbox_inches='tight', pad_inches=0.1)
+    plt.show(block=False)
+    plt.pause(2)
+    plt.close()
+    print("Done")
 
 if __name__=="__main__":
-    if PLOT_ADULT:
-        analysis_drop_correlated_features(create_adult_dataset, os.path.join("dataset_analysis_plots", "DROP"), "adult.png")
-    if PLOT_COMPAS:
-        analysis_drop_correlated_features(create_compas_dataset, os.path.join("dataset_analysis_plots", "DROP"), "compas.png")
-    if PLOT_TITANIC:
-        analysis_drop_correlated_features(create_titanic_dataset, os.path.join("dataset_analysis_plots", "DROP"), "titanic.png")
-    if PLOT_GERMAN:
-        analysis_drop_correlated_features(create_german_dataset, os.path.join("dataset_analysis_plots", "DROP"), "german.png")
-    if PLOT_BANK:
-        analysis_drop_correlated_features(create_bank_dataset, os.path.join("dataset_analysis_plots", "DROP"), "bank.png")
-    if PLOT_COMMUNITIES:
-        analysis_drop_correlated_features(create_communities_dataset, os.path.join("dataset_analysis_plots", "DROP"), "communities.png")
+    if PLOT_DROP:
+        if PLOT_ADULT:
+            analysis_drop_correlated_features(create_adult_dataset, os.path.join("dataset_analysis_plots", "DROP"), "adult.png")
+        if PLOT_COMPAS:
+            analysis_drop_correlated_features(create_compas_dataset, os.path.join("dataset_analysis_plots", "DROP"), "compas.png")
+        if PLOT_TITANIC:
+            analysis_drop_correlated_features(create_titanic_dataset, os.path.join("dataset_analysis_plots", "DROP"), "titanic.png")
+        if PLOT_GERMAN:
+            analysis_drop_correlated_features(create_german_dataset, os.path.join("dataset_analysis_plots", "DROP"), "german.png")
+        if PLOT_BANK:
+            analysis_drop_correlated_features(create_bank_dataset, os.path.join("dataset_analysis_plots", "DROP"), "bank.png")
+        if PLOT_COMMUNITIES:
+            analysis_drop_correlated_features(create_communities_dataset, os.path.join("dataset_analysis_plots", "DROP"), "communities.png")
+    if PLOT_IMPUTE:
+        if PLOT_ADULT:
+            analysis_impute_correlated_features(create_adult_dataset, os.path.join("dataset_analysis_plots", "IMPUTE"), "adult.png")
+        if PLOT_COMPAS:
+            analysis_impute_correlated_features(create_compas_dataset, os.path.join("dataset_analysis_plots", "IMPUTE"), "compas.png")
+        if PLOT_TITANIC:
+            analysis_impute_correlated_features(create_titanic_dataset, os.path.join("dataset_analysis_plots", "IMPUTE"), "titanic.png")
+        if PLOT_GERMAN:
+            analysis_impute_correlated_features(create_german_dataset, os.path.join("dataset_analysis_plots", "IMPUTE"), "german.png")
+        if PLOT_BANK:
+            analysis_impute_correlated_features(create_bank_dataset, os.path.join("dataset_analysis_plots", "IMPUTE"), "bank.png")
+        if PLOT_COMMUNITIES:
+            analysis_impute_correlated_features(create_communities_dataset, os.path.join("dataset_analysis_plots", "IMPUTE"), "communities.png")
